33fabcdc10627ba83ac169f78ebf3612beab3831
TalHek <tal1234@gmail.com>
2017-09-16 21:46:05 +0300

    added UTankTurret class for turret rotation

---
+++ BattleTank/Source/BattleTank/Private/TankTurrent.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/TankTurrent.h"
+#include "Engine/World.h"
+
+void UTankTurrent::Rotate(float RelativeSpeed) {
+	//need to move it from one to another
+	//add rotator and each frame, move by x cm until getting to target position
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1.0f, +1.0f);
+	auto RotationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto Rotation = RelativeRotation.Yaw + RotationChange;
+	SetRelativeRotation(FRotator(0, Rotation, 0));
+	UE_LOG(LogTemp, Warning, TEXT("%f Turrent Elevation "), Rotation);
+}
+
+
