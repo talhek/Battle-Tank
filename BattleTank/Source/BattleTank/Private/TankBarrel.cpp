b96c779e1a20bc32ad6f2c8b9d99282e762126d4
TalHek <tal1234@gmail.com>
2017-09-16 16:24:42 +0300

    [Class] added UTankBarrel - new staticmesh component for control of the barrel movement

---
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/TankBarrel.h"
+#include "Engine/World.h"
+#include "Components/StaticMeshComponent.h"
+
+void UTankBarrel::Elevate(float RelativeSpeed) {
+
+	//need to move it from one to another
+	//add rotator and each frame, move by x cm until getting to target position
+
+
+
+}
+
+
---
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankBarrel.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+public:
+	// -1 is max downward speed, and +1 is max up speed
+	void Elevate(float RelativeSpeed);
+
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MinElevationDegrees = 0; 
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxElevationDegrees = 40; 
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxDegreesPerSecond = 5;
+
+	
+	
+	
+	
+};
